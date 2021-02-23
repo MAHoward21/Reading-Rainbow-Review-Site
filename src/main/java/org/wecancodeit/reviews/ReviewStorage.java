4f877a4d33906f42797845a8e549db1baf12f0c0
Connor <connor@chamfam.net>
2021-02-23 14:46:56 -0500

    Removed dependecies from ReviewController

--- src/main/java/org/wecancodeit/reviews/ReviewStorage.java
+++ src/main/java/org/wecancodeit/reviews/ReviewStorage.java
@@ -2,6 +2,9 @@ package org.wecancodeit.reviews;
 
 
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 //Store Review objects
 //Provide a way to save new reviews
 //Provide a way to retrieve reviews
@@ -12,19 +15,25 @@ import org.springframework.stereotype.Service;
 public class ReviewStorage {
 
 
-    private Catagory sampleCatagory;
-    private Review sample;
+
+    Map<String, Review> reviews;
+
+
     public ReviewStorage(){
-        sampleCatagory = new Catagory("Kids books", "The books you read to kids who cant talk");
-        sample = new Review("Where is Spot?", "Eric Hill", "https://target.scene7.com/is/image/Target/GUEST_5d5829f2-b8f2-4e6a-a4e2-c930457cff1d?wid=488&hei=488&fmt=pjpeg",
+
+        reviews = new HashMap<>();
+        Catagory sampleCatagory = new Catagory("Kids books", "The books you read to kids who cant talk");
+        Review sample = new Review("Where is Spot", "Eric Hill", "https://target.scene7.com/is/image/Target/GUEST_5d5829f2-b8f2-4e6a-a4e2-c930457cff1d?wid=488&hei=488&fmt=pjpeg",
                 sampleCatagory,
                 "This book has fun flaps to lift and lots of anilams to name with you child. would suggest this book for colors and counting also.");
+
+        reviews.put(sample.getReviewTitle(), sample);
     }
 
 
 
 
-    public Review getSample() {
-        return sample;
+    public Review getSamplebyTitle(String title) {
+        return reviews.get(title);
     }
 }
