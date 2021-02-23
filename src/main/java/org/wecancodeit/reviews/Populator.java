c08dee3c2c9704f283d81fe227f841a5cc876455
Michele Howard <azariahsmommy15@gmail.com>
2021-02-23 15:19:24 -0500

    "Working on building the Populator. At a stand-still where the localhost:8080 isn't working. Thinking it may be because the Populator and the ReviewStorage are not yet connected"

---
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -0,0 +1,20 @@
+package org.wecancodeit.reviews;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Populator implements CommandLineRunner {
+
+    private ReviewStorage reviewStorage;
+
+    public Populator(ReviewStorage reviewStorage){
+
+        this.reviewStorage = reviewStorage;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    Catagory kidsBooks   = new Catagory("The Kids Books", "It's books about kid stuff.");
+    Review kidsBooksReview = new Review("Where is Spot", "Eric Hill", "https://target.scene7.com/is/image/Target/GUEST_5d5829f2-b8f2-4e6a-a4e2-c930457cff1d?wid=488&hei=488&fmt=pjpeg",
+            kidsBooks,  "This book has fun flaps to lift and lots of anilams to name with you child. would suggest this book for colors and counting also.");
+    }
+}
