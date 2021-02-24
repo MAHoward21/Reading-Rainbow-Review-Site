3170f407ddfe642ece5f52bb0ac53b7d30b59d70
miguel garcia <sinxash8@gmail.com>
2021-02-24 15:20:07 -0500

    worked on category controller and storage.

--- src/main/java/org/wecancodeit/reviews/CatagoryStorage.java
+++ src/main/java/org/wecancodeit/reviews/CatagoryStorage.java
@@ -5,30 +5,30 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @Service
 public class CatagoryStorage {
-    Map<String, Catagory> catogory;
-//    private Catagory sampleCatagory;
-//    private Review sample;
+    private Catagory sampleCatagory;
+    private Review sample;
+
+    Map<String, Catagory> catagories;
 
     public CatagoryStorage() {
-        catogory = new HashMap<>();
-//        sampleCatagory = new Catagory("Kids books", "The books you read to kids who cant talk");
-//        Review sample = new Review("Where is Spot?", "Eric Hill", "https://target.scene7.com/is/image/Target/GUEST_5d5829f2-b8f2-4e6a-a4e2-c930457cff1d?wid=488&hei=488&fmt=pjpeg",
-//                sampleCatagory,
-//                "This book has fun flaps to lift and lots of anilams to name with you child. would suggest this book for colors and counting also.");
-//    }
-//    reviews.put(sample.getName(),sampleCatagory);
+        catagories = new HashMap<>();
 
-        public void saveReview(Catagory catogory){
-            catogory.put(catogory.getCatagoryDescription(),catogory);
-        }
+        Catagory sampleCatagory = new Catagory("Kids books", "The books you read to kids who cant talk");
 
-        public Review getSamplebyName(String description) {
-            return catogory.get(name);
-        }
-}
+        catagories.put(sampleCatagory.getName(), sampleCatagory);
+    }
+
+    public void saveCatagory(Catagory catagory){
+        catagories.put(catagory.getName(), catagory);
+    }
+
+
+
+    public Catagory getSample(String name) {
+        return catagories.get(name);
+    }
 }
 
 
