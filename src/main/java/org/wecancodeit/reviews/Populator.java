d7a36c232190cc6f07ada1952c8e80a75b549a2f
Connor <connor@chamfam.net>
2021-03-03 11:44:03 -0500

    added fathi's populator data

--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -22,22 +22,37 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-    Book harryPotter = new Book("Harry Potter", "JKR", "#");
-    bookStorage.addBook(harryPotter);
 
-    Book wheresSpot = new Book("Where's Spot", "Eric Hill", "https://target.scene7.com/is/image/Target/GUEST_5d5829f2-b8f2-4e6a-a4e2-c930457cff1d?wid=488&hei=488&fmt=pjpeg");
-    bookStorage.addBook(wheresSpot);
+        Category fantasy = new Category("fantasy", "Books with magic and unicorns.");
+        Category kidsBooks = new Category("kidsbooks", "It's books about kid stuf.");
+        Category mystery = new Category("mystery", "Books about some type of crime");
+        Category poetry = new Category("poetry","Books about poetry");
+        categoryStorage.addCategory(fantasy);
+        categoryStorage.addCategory(kidsBooks);
+        categoryStorage.addCategory(mystery);
+        categoryStorage.addCategory(poetry);
 
-    Review harryPotterReview = new Review("This book is Good", "Jim Bob", "#", "kidsBooks", "This book is so good i laughed");
-    reviewStorage.addReview(harryPotterReview);
+        Book harryPotter = new Book("Harry Potter", "JKR", "#");
+        bookStorage.addBook(harryPotter);
+        Book wheresSpot = new Book("Where's Spot", "Eric Hill", "https://target.scene7.com/is/image/Target/GUEST_5d5829f2-b8f2-4e6a-a4e2-c930457cff1d?wid=488&hei=488&fmt=pjpeg");
+        bookStorage.addBook(wheresSpot);
+        Book nancyDrew = new Book("Nancy Drew", "Carolyn Keene", "#");
+        bookStorage.addBook(nancyDrew);
+        Book whereTheSidewalkEnds = new Book("Where The Sidewalk Ends","Shel silverstein", "#");
+        bookStorage.addBook(whereTheSidewalkEnds);
 
-    harryPotter.addReview(harryPotterReview);
-
-    Category kidsBooks   = new Category("kidsBooks", "It's books about kid stuff.");
-    categoryStorage.addCategory(kidsBooks);
-
-    Category fantasy = new Category("Fantasy", "Books with magic and unicorns.");
-    categoryStorage.addCategory(fantasy);
+        Review harryPotterReview = new Review("This book is Good", "Jim Bob", "#", "fantasy", "This book is so good I laughed");
+        reviewStorage.addReview(harryPotterReview);
+        harryPotter.addReview(harryPotterReview);
+        Review wheresSpotReview = new Review("Kids will like reading this book", "Jane Lee", "#", "kidsBooks", "amazing book");
+        reviewStorage.addReview(wheresSpotReview);
+        wheresSpot.addReview(wheresSpotReview);
+        Review nancyDrewReview = new Review("This book is great!", "Lee Bob", "#", "mystery", "My head still spinning from reading this book");
+        reviewStorage.addReview(nancyDrewReview);
+        nancyDrew.addReview(nancyDrewReview);
+        Review whereTheSideWalkEnds = new Review("such a pleasure to read this book!", "Jonny Bob", "#", "poetry", "Interesting book");
+        reviewStorage.addReview(whereTheSideWalkEnds);
+        whereTheSidewalkEnds.addReview(whereTheSideWalkEnds);
 
 
 
